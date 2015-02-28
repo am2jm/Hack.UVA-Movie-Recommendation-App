4e8426fa192f530bdabd48f45d1de2f91d4255cf
Divya354 <dsk5df@virginia.edu>
2015-02-28 16:44:21 -0500

    LoadsMovies
    Theater.java
    Won’t work until Movie works
    We need to plan out methods for this class

---
+++ Theater.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class Theater {
+	private ArrayList<Movie> theater;
+
+	public Theater() throws Exception {
+		// fix to try/catch
+		theater= new ArrayList<Movie>();
+		for (int i = 1; i < 1683; i++) {
+			Movie m = new Movie(i);
+			theater.add(m);
+		}
+	}
+
+	public ArrayList<Movie> getTheater() {
+		return theater;
+	}
+	//public Movie getMovie(String title){ //method to search theater by name 
+		//for(int i = 0; i< 1682; i++){
+			//if
+		//}
+		//return null;	
+	//}
+	
+}
